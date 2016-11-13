38a0b18a4b275f600a5c33fbf2b63730909107fb
Sean Scully <sean.erik.scully@gmail.com>
2016-11-13 21:48:21 +0100

    (test) introduced a bug to fix as an exercise

--- tests/api/events.spec.js
+++ tests/api/events.spec.js
@@ -22,7 +22,7 @@ describe('Events', () => {
                 .get(endpointBase)
                 .end((err, res) => {
                     res.should.have.status(200);
-                    res.body.should.be.a('array');
+                    res.body.should.be.a('object');
                     res.body.length.should.be.eql(0);
                     done();
                 });
