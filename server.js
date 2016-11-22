d2aa5b210c34f9d00b4972d1648d3190bd345de0
Sean Scully <sean.erik.scully@gmail.com>
2016-11-22 22:54:25 +0100

    misc renaming

--- server.js
+++ server.js
@@ -9,17 +9,16 @@ import flash from 'connect-flash';
 import config from './config/config';
 import passportConfig from './config/passport';
 
-import getEventsRouter from './src/api/event/routes/get_events';
-import postEventsrouter from './src/api/event/routes/post_events';
-import postUser from './src/api/profile/routes/post_user';
-import getValidateUser from './src/api/profile/routes/get_validate_user';
+import eventsGetRouter from './src/api/event/routes/get_events';
+import eventsPostRouter from './src/api/event/routes/post_events';
+import userPostRouter from './src/api/profile/routes/post_user';
+import validateUserRouter from './src/api/profile/routes/get_validate_user';
 
 const app = express();
 const port = process.env.PORT || 1337;
 
 mongoose.Promise = Promise;
-app.use(bodyParser.urlencoded({extended:true}));
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 // required for passport
 app.use(session({ 
@@ -33,13 +32,13 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 //routes
-app.use('/api/events', getEventsRouter);
-app.use('/api/events', postEventsrouter);
-app.use('/api/user', postUser);
-app.use('/api/validateUser', getValidateUser);
+app.use('/api/events', eventsGetRouter);
+app.use('/api/events', eventsPostRouter);
+app.use('/api/user', userPostRouter);
+app.use('/api/validateUser', validateUserRouter);
 
 //kickstart server
-app.listen(port, () =>{
+app.listen(port, () => {
 
     mongoose.connect(config.db, config.dbOptions, (err) =>{
         if(err){
