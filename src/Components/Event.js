2cf4c2321a65962bc0beb2d6b0c0155623a056a7
Sean Scully <sean.erik.scully@gmail.com>
2017-01-17 19:28:25 +0100

    adds better resources for exercises

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 /* Todo: this is not very useful now, suggestions to improvements as an exercise
-  1. fetch event (connect it to store!)
   2. render useful information about the event
   3. are you e.g. registered to this event?
   4. if you are an admin to this event, maybe you should be able to modify it, too?
+  5. later (redux exercise) - fetch event with eventId
  */
 class Event extends React.Component {
   render() {
