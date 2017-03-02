bdc43d5b85ff6605d38211a215af5a0d504f5d90
Sean Scully <sean.erik.scully@gmail.com>
2017-03-02 19:34:09 +0100

    make login route look lovely

--- src/Routes/Login.js
+++ src/Routes/Login.js
@@ -2,24 +2,52 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { UserActions } from '../Actions/';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
 
 class Login extends Component {
 
   handleLogin(event) {
     event.preventDefault();
     const { email, password } = this.refs;
-    this.props.dispatch(UserActions.login(email.value, password.value));
+    this.props.dispatch(UserActions.login(email.getValue(), password.getValue()));
   }
 
   render() {
+
+    const inputStyle = {
+      width: '100%'
+    }
+
     return (
       <div>
-        <form className="form" autoComplete="off" onSubmit={(event) => this.handleLogin(event)}>
-          <input ref="email" type="email" className="form-control" name="email" placeholder="Username" />
-          <input ref="password" type="password" className="form-control" name="pw" placeholder="Password" />
-          <button type="submit" id="login-button">Login</button>
+        <form className="login-form" autoComplete="off" onSubmit={(event) => this.handleLogin(event)}>
+          <TextField
+            id="email"
+            ref="email"
+            defaultValue={""}
+            style={inputStyle}
+            name="email"
+            floatingLabelFixed
+            floatingLabelText="E-mail"
+          />
+          <TextField
+            id="password"
+            style={inputStyle}
+            ref="password"
+            defaultValue={""}
+            type="password"
+            name="password"
+            floatingLabelFixed
+            floatingLabelText="Password"
+          />
+          <RaisedButton primary type="submit" id="login-button" onClick={this.handleLogin.bind(this)} label="Login"/>
         </form>
-        <Link to="/signup">Sign up</Link>
+        <div style={{marginTop: 20}}>
+          <Link to="/forgot" style={{marginRight: 20}}>Forgot password?</Link>
+          <Link to="/signup">Sign up</Link>
+        </div>
       </div>
     )
   }
