40b61fa4fa92d2234486cba0431b2b11461a3999
Sean Scully <sean.erik.scully@gmail.com>
2017-03-06 17:48:49 +0100

    add arrow-back to footer, minor adjustments to style

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { browserHistory } from 'react-router';
+import MdArrowBack from 'material-ui/svg-icons/navigation/arrow-back'
+
 
 const Footer = props => (
   <div className="footer">
+    <MdArrowBack color="#fff" onClick={() => browserHistory.goBack() } />
     <div className="copyright">(c) 2017</div>
   </div>
 )
