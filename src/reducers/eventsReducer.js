21f85257fb12596ff36f1be6f89c3f34deddaa89
Sean Scully <sean.erik.scully@gmail.com>
2017-03-06 17:41:15 +0100

    fix subscribe-unsubscribe

--- src/reducers/eventsReducer.js
+++ src/reducers/eventsReducer.js
@@ -11,10 +11,22 @@ export default (state = initialState, action) => {
         data: action.payLoad
       });
 
-      case 'RECEIVED_LOCATIONS':
+    case 'RECEIVED_LOCATIONS':
       return Object.assign({}, state, {
         locations: action.payLoad
       });
+
+    case 'UPDATE_EVENT':
+      const updatedEvent = action.payLoad
+      let data  = state.data.slice()
+      return Object.assign({}, state, {
+        data: data.map( event => {
+          if (event._id === updatedEvent._id) {
+            return updatedEvent
+          }
+          return event
+        })
+      })
 
     default: return state;
   }
